c136354f65a49d85b1caa9a213cd4230e4d044e7
ELANGOVAN. B <147794775+githubelan77@users.noreply.github.com>
2025-08-04 10:40:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce/',
+  base: '/ecomJenkinspractice/',
 })
+// ecomJenkinspractice/:11   GET http://localhost:9090/ecommerce/assets/index-D8wDBqOF.js net::ERR_ABORTED 404 (Not Found)
